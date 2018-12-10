edfa1916f62c556a3d10610074a7fa498a80a479
Cauzzi Marco <marco.cauzzi@beta80group.it>
2018-12-10 12:46:56 +0100

    -Kindergarten completed

--- kindergarten-garden/KindergartenGarden.cs
+++ kindergarten-garden/KindergartenGarden.cs
@@ -11,12 +11,42 @@ public enum Plant
 
 public class KindergartenGarden
 {    
+    public List<string> studentsList {get; private set;} = new List<string>(){"Alice","Bob", "Charlie", "David",
+                                      "Eve", "Fred", "Ginny", "Harriet",
+                                      "Ileana", "Joseph", "Kincaid", "Larry"};
+    public List<string> rows {get; private set;} = new List<string>();
     public KindergartenGarden(string diagram)
     {
+        rows = new List<string>(diagram.Split('\n'));
     }
 
     public IEnumerable<Plant> Plants(string student)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var studentPlants=rows[0].Substring(studentsList.IndexOf(student)*2, 2)+rows[1].Substring(studentsList.IndexOf(student)*2, 2);
+        var plantsList=new List<Plant>();
+        studentPlants=studentPlants.ToLower();
+
+        foreach(char c in studentPlants){
+            switch(c){
+                case 'g':
+                    plantsList.Add(Plant.Grass);
+                    break;
+                case 'v':
+                    plantsList.Add(Plant.Violets);
+                    break;
+                case 'c':
+                    plantsList.Add(Plant.Clover);
+                    break;
+                case 'r':
+                    plantsList.Add(Plant.Radishes);
+                    break;
+                default:
+                    throw new ArgumentException();
+                    break;
+            }
+        }
+
+        //plantsList.Sort();
+        return plantsList;
     }
 }
