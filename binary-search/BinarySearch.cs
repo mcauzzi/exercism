ea1b73e8ff1f77e48eed496f4a183294466ffbd0
Cauzzi Marco <marco.cauzzi@beta80group.it>
2018-12-13 09:03:06 +0100

    Added binary search

---
+++ binary-search/BinarySearch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public static class BinarySearch
+{
+    public static int Find(int[] input, int value)
+    {
+        if(input.Length==0){
+            return -1;
+        }
+        int l=0;
+        int r=input.Length-1;
+        for(int i=(l+r)/2;l<=r;){   
+
+            if(value>input[i]){
+                l=i+1;
+            } else if(value<input[i]){
+                r=i-1;
+            }else if(value==input[i]){
+                return i;
+            }
+
+            i=(int)Math.Floor( (double)(l+r)/2);
+        }
+
+        return -1;
+    }
+}
