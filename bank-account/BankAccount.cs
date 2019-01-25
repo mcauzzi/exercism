5134cc678113d9bc5b9642a07def372a37f89794
mcauzzi <marcosfurai93>
2019-01-25 10:30:28 +0100

    -Added Bank Account

---
+++ bank-account/BankAccount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+public class BankAccount
+{
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK here.")]
+    private decimal balance;
+
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK here.")]
+    private bool isOpen;
+
+    private readonly object sem = new object();
+
+    public void Open()
+    {
+        Balance = 0;
+        this.isOpen = true;
+    }
+
+    public void Close()
+    {
+        this.isOpen = false;
+    }
+
+    public decimal Balance
+    {
+        get
+        {
+            if (this.isOpen)
+            {
+                return this.balance;
+            }
+            else
+            {
+                throw new InvalidOperationException("Account Closed");
+            }
+        }
+
+        private set => this.balance = value;
+    }
+
+    public void UpdateBalance(decimal change)
+    {
+
+        lock (sem)
+        {
+            if (this.isOpen)
+            {
+                this.Balance += change;
+            }
+            else
+            {
+                throw new InvalidOperationException("Account Closed");
+            }
+        }
+    }
+}
